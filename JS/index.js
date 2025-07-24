50a1ed76dc8482e232b2f805ecd29540321840b3
AndreFirmansyah06 <indomieseleramu1@gmail>
2025-07-24 21:14:01 +0700

    view(done slicing):kurang mengisi data sebenarnya

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
-function toggleMobile(imgEl) {
+function toggleMobile(imgEl, gambar) {
   // Cek apakah ini layar mobile
-  if (window.innerWidth < 768) {
+  if (window.innerWidth <= 768) {
     const infoEl = imgEl.parentElement.querySelector("[data-info]");
 
     infoEl.classList.toggle("opacity-0");
@@ -11,5 +11,11 @@ function toggleMobile(imgEl) {
       infoEl.classList.contains("opacity-0") ? "false" : "true"
     );
   }
-
+  if (window.innerWidth <= 768) {
+    const blurImage = imgEl.closest(".group");
+    const capture = blurImage.querySelector(".gambar");
+    // console.log(capture);
+    capture.classList.toggle("blur-xs");
+  }
 }
+
