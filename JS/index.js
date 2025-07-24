7da604c2ea38f8cba96473946ce73929ea7b89c5
AndreFirmansyah06 <indomieseleramu1@gmail.com>
2025-07-24 16:06:45 +0700

    a

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,14 @@ function toggleMobile(imgEl) {
   // Cek apakah ini layar mobile
   if (window.innerWidth < 768) {
     const infoEl = imgEl.parentElement.querySelector("[data-info]");
-    infoEl.classList.toggle("hidden");
+
+    infoEl.classList.toggle("opacity-0");
+    infoEl.classList.toggle("translate-y-2");
+    infoEl.classList.toggle("pointer-events-none");
+    infoEl.setAttribute(
+      "data-active",
+      infoEl.classList.contains("opacity-0") ? "false" : "true"
+    );
   }
+
 }
