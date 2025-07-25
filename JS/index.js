6924c1b4de2876dc85f18762cf65d160c2059498
AndreFirmansyah06 <indomieseleramu1@gmail.com>
2025-07-25 10:12:23 +0700

    c

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,19 @@
 function toggleMobile(imgEl) {
   // Cek apakah ini layar mobile
-  if (window.innerWidth <= 768) {
-    const infoEl = imgEl.parentElement.querySelector("[data-info]");
 
-    infoEl.classList.toggle("opacity-0");
-    infoEl.classList.toggle("translate-y-2");
-    infoEl.classList.toggle("pointer-events-none");
-    infoEl.setAttribute(
-      "data-active",
-      infoEl.classList.contains("opacity-0") ? "false" : "true"
-    );
+  const infoEl = imgEl.parentElement.querySelector("[data-info]");
 
-    const blurImage = imgEl.closest(".group");
-    const capture = blurImage.querySelector(".gambar");
-    if (capture) {
-      capture.classList.toggle("blur-xs");
-    }
+  infoEl.classList.toggle("opacity-0");
+  infoEl.classList.toggle("translate-y-2");
+  infoEl.classList.toggle("pointer-events-none");
+  infoEl.setAttribute(
+    "data-active",
+    infoEl.classList.contains("opacity-0") ? "false" : "true"
+  );
+
+  const blurImage = imgEl.closest(".group");
+  const capture = blurImage.querySelector(".gambar");
+  if (capture) {
+    capture.classList.toggle("blur-xs");
   }
 }
